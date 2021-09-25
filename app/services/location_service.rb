601d3ca5101edde5d4d0c53217250399776aaf5d
Scott Borecki <scottborecki@gmail.com>
2021-09-25 11:42:59 -0600

    Create LocationService and .get_coordinates

---
+++ app/services/location_service.rb
@@ -0,0 +1,17 @@
+class LocationService
+  def self.conn
+    Faraday.new(
+      url: 'http://www.mapquestapi.com',
+      params: { key: ENV['mapquest_api_key'] },
+      headers: { 'Content-Type' => 'application/json' }
+    )
+  end
+
+  def self.get_coordinates(location)
+    response = conn.get('/geocoding/v1/address') do |req|
+      req.params['location'] = location
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
