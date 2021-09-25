f9f12c44d967f4251cbd1525bff737a198c0f202
Scott Borecki <scottborecki@gmail.com>
2021-09-25 16:29:52 -0600

    Create ForecastService

---
+++ app/services/forecast_service.rb
@@ -0,0 +1,23 @@
+class ForecastService
+  def self.conn
+    Faraday.new(
+      url: 'https://api.openweathermap.org',
+      params: { appid: ENV['open_weather_api_key'] },
+      headers: { 'Content-Type' => 'application/json' }
+    )
+  end
+
+  # :exclude options: 'current,minutely,hourly,daily,alerts'
+  # :units options:   'standard,metric,imperial'
+  def self.get_forecast(exclude: 'minutely,alerts', units: 'imperial',
+                        **arguments)
+    response = conn.get('/data/2.5/onecall') do |req|
+      req.params['lat']     = arguments[:lat]
+      req.params['lon']     = arguments[:lon]
+      req.params['exclude'] = arguments[:exclude] || exclude
+      req.params['units']   = arguments[:units] || units
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
---
+++ spec/services/forecast_service_spec.rb
@@ -0,0 +1,117 @@
+require 'rails_helper'
+
+describe ForecastService, type: :service do
+  describe 'class methods' do
+    describe '.get_forecast' do
+      context 'when I provide valid coordinates', :vcr do
+        subject(:forecast) { ForecastService.get_forecast(coordinates) }
+
+        let(:coordinates) { { lat: 39.738453, lon: -104.984853 } }
+
+        it 'returns the forecast as a hash', :aggregate_failures do
+          expect(forecast).to be_a(Hash)
+
+          expect(forecast).to have_key(:lat)
+          expect(forecast[:lat]).to eq(coordinates[:lat].round(4))
+
+          expect(forecast).to have_key(:lon)
+          expect(forecast[:lon]).to eq(coordinates[:lon].round(4))
+
+          expect(forecast).to have_key(:current)
+          expect(forecast[:current]).to be_a(Hash)
+
+          expect(forecast[:current]).to have_key(:dt)
+          expect(forecast[:current][:dt]).to be_an(Integer)
+
+          expect(forecast[:current]).to have_key(:sunrise)
+          expect(forecast[:current][:sunrise]).to be_an(Integer)
+
+          expect(forecast[:current]).to have_key(:sunrise)
+          expect(forecast[:current][:sunrise]).to be_an(Integer)
+
+          expect(forecast[:current]).to have_key(:temp)
+          expect(forecast[:current][:temp]).to be_a(Float)
+
+          expect(forecast[:current]).to have_key(:feels_like)
+          expect(forecast[:current][:feels_like]).to be_a(Float)
+
+          expect(forecast[:current]).to have_key(:humidity)
+          expect(forecast[:current][:humidity]).to be_an(Integer)
+
+          expect(forecast[:current]).to have_key(:uvi)
+          expect(forecast[:current][:uvi]).to be_a(Float)
+
+          expect(forecast[:current]).to have_key(:visibility)
+          expect(forecast[:current][:visibility]).to be_an(Integer)
+
+          expect(forecast[:current]).to have_key(:weather)
+          expect(forecast[:current][:weather]).to be_an(Array)
+          expect(forecast[:current][:weather].first).to be_a(Hash)
+
+          expect(forecast[:current][:weather].first).to have_key(:description)
+          expect(forecast[:current][:weather].first[:description]).to be_a(String)
+
+          expect(forecast[:current][:weather].first).to have_key(:icon)
+          expect(forecast[:current][:weather].first[:icon]).to be_a(String)
+
+          expect(forecast).to have_key(:daily)
+          expect(forecast[:daily]).to be_an(Array)
+          expect(forecast[:daily].first).to be_a(Hash)
+
+          expect(forecast[:daily].first).to have_key(:dt)
+          expect(forecast[:daily].first[:dt]).to be_an(Integer)
+
+          expect(forecast[:daily].first).to have_key(:sunrise)
+          expect(forecast[:daily].first[:sunrise]).to be_an(Integer)
+
+          expect(forecast[:daily].first).to have_key(:sunrise)
+          expect(forecast[:daily].first[:sunrise]).to be_an(Integer)
+
+          expect(forecast[:daily].first).to have_key(:temp)
+          expect(forecast[:daily].first[:temp]).to be_a(Hash)
+
+          expect(forecast[:daily].first[:temp]).to have_key(:max)
+          expect(forecast[:daily].first[:temp][:max]).to be_a(Float)
+
+          expect(forecast[:daily].first[:temp]).to have_key(:min)
+          expect(forecast[:daily].first[:temp][:min]).to be_a(Float)
+
+          expect(forecast[:daily].first[:weather].first).to have_key(:description)
+          expect(forecast[:daily].first[:weather].first[:description]).to be_a(String)
+
+          expect(forecast[:daily].first[:weather].first).to have_key(:icon)
+          expect(forecast[:daily].first[:weather].first[:icon]).to be_a(String)
+
+
+          expect(forecast).to have_key(:hourly)
+          expect(forecast[:hourly]).to be_an(Array)
+          expect(forecast[:hourly].first).to be_a(Hash)
+
+          expect(forecast[:hourly].first).to have_key(:dt)
+          expect(forecast[:hourly].first[:dt]).to be_an(Integer)
+
+          expect(forecast[:hourly].first).to have_key(:temp)
+          expect(forecast[:hourly].first[:temp]).to be_a(Float)
+
+          expect(forecast[:hourly].first[:weather].first).to have_key(:description)
+          expect(forecast[:hourly].first[:weather].first[:description]).to be_a(String)
+
+          expect(forecast[:hourly].first[:weather].first).to have_key(:icon)
+          expect(forecast[:hourly].first[:weather].first[:icon]).to be_a(String)
+        end
+      end
+
+      context 'when I do not provide coordinates', :vcr do
+        subject(:response) { ForecastService.get_forecast(empty_coordinates) }
+
+        let(:empty_coordinates) { { lat: '', lon: '' } }
+
+        it 'returns the error details as a hash', :aggregate_failures do
+          expect(response).to be_a(Hash)
+          expect(response[:cod]).to eq('400')
+          expect(response[:message]).to eq('Nothing to geocode')
+        end
+      end
+    end
+  end
+end
