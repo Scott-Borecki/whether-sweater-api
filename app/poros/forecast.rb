90e2cc69bce6f0b43b636b3e2c8b1718194ca5d3
Scott Borecki <scottborecki@gmail.com>
2021-09-25 09:31:09 -0600

    Create Forecast poro

---
+++ app/poros/forecast.rb
@@ -0,0 +1,53 @@
+class Forecast
+  attr_reader :current_weather, :daily_weather, :hourly_weather
+
+  def initialize(data)
+    @current_weather = populate_current_weather(data)
+    @daily_weather   = populate_daily_weather(data)
+    @hourly_weather  = populate_hourly_weather(data)
+  end
+
+  def id
+    nil
+  end
+
+  def populate_current_weather(data)
+    {
+      datetime:    Time.zone.at(data[:current][:dt]).to_s,
+      sunrise:     Time.zone.at(data[:current][:sunrise]).to_s,
+      sunset:      Time.zone.at(data[:current][:sunset]).to_s,
+      temperature: data[:current][:temp],
+      feels_like:  data[:current][:feels_like],
+      humidity:    data[:current][:humidity],
+      uvi:         data[:current][:uvi],
+      visibility:  data[:current][:visibility],
+      conditions:  data[:current][:weather].first[:description],
+      icon:        data[:current][:weather].first[:icon]
+    }
+  end
+
+  def populate_daily_weather(data)
+    data[:daily].map do |day|
+      {
+        date:       Time.zone.at(day[:dt]).strftime('%Y-%m-%d'),
+        sunrise:    Time.zone.at(day[:sunrise]).to_s,
+        sunset:     Time.zone.at(day[:sunset]).to_s,
+        max_temp:   day[:temp][:max],
+        min_temp:   day[:temp][:min],
+        conditions: day[:weather].first[:description],
+        icon:       day[:weather].first[:icon]
+      }
+    end
+  end
+
+  def populate_hourly_weather(data)
+    data[:hourly].map do |hour|
+      {
+        time:        Time.zone.at(hour[:dt]).to_s(:time),
+        temperature: hour[:temp],
+        conditions:  hour[:weather].first[:description],
+        icon:        hour[:weather].first[:icon]
+      }
+    end
+  end
+end
---
+++ spec/poros/forecast_spec.rb
@@ -0,0 +1,86 @@
+require 'rails_helper'
+
+describe Forecast, type: :poro do
+  describe 'object creation' do
+    # See spec/support/forecast_response_body.rb for test setup helper method:
+    #   #forecast_imperial_response_body
+    subject(:forecast) { Forecast.new(forecast_imperial_response_body) }
+
+    it 'is valid and has readable attributes', :aggregate_failures do
+      expect(forecast).to be_a(Forecast)
+      expect(forecast.current_weather).to be_a(Hash)
+      expect(forecast.current_weather.size).to eq(10)
+
+      expect(forecast.current_weather).to have_key(:datetime)
+      expect(forecast.current_weather[:datetime]).to be_a(String)
+
+      expect(forecast.current_weather).to have_key(:sunrise)
+      expect(forecast.current_weather[:sunrise]).to be_a(String)
+
+      expect(forecast.current_weather).to have_key(:sunset)
+      expect(forecast.current_weather[:sunset]).to be_a(String)
+
+      expect(forecast.current_weather).to have_key(:temperature)
+      expect(forecast.current_weather[:temperature]).to be_a(Float)
+
+      expect(forecast.current_weather).to have_key(:feels_like)
+      expect(forecast.current_weather[:feels_like]).to be_a(Float)
+
+      expect(forecast.current_weather).to have_key(:humidity)
+      expect(forecast.current_weather[:humidity]).to be_a(Numeric)
+
+      expect(forecast.current_weather).to have_key(:uvi)
+      expect(forecast.current_weather[:uvi]).to be_a(Numeric)
+
+      expect(forecast.current_weather).to have_key(:visibility)
+      expect(forecast.current_weather[:visibility]).to be_a(Numeric)
+
+      expect(forecast.current_weather).to have_key(:conditions)
+      expect(forecast.current_weather[:conditions]).to be_a(String)
+
+      expect(forecast.current_weather).to have_key(:icon)
+      expect(forecast.current_weather[:icon]).to be_a(String)
+
+      expect(forecast.daily_weather).to be_an(Array)
+      expect(forecast.daily_weather.first).to be_a(Hash)
+      expect(forecast.daily_weather.first.size).to eq(7)
+
+      expect(forecast.daily_weather.first).to have_key(:date)
+      expect(forecast.daily_weather.first[:date]).to be_a(String)
+
+      expect(forecast.daily_weather.first).to have_key(:sunrise)
+      expect(forecast.daily_weather.first[:sunrise]).to be_a(String)
+
+      expect(forecast.daily_weather.first).to have_key(:sunset)
+      expect(forecast.daily_weather.first[:sunset]).to be_a(String)
+
+      expect(forecast.daily_weather.first).to have_key(:max_temp)
+      expect(forecast.daily_weather.first[:max_temp]).to be_a(Float)
+
+      expect(forecast.daily_weather.first).to have_key(:min_temp)
+      expect(forecast.daily_weather.first[:min_temp]).to be_a(Float)
+
+      expect(forecast.daily_weather.first).to have_key(:conditions)
+      expect(forecast.daily_weather.first[:conditions]).to be_a(String)
+
+      expect(forecast.daily_weather.first).to have_key(:icon)
+      expect(forecast.daily_weather.first[:icon]).to be_a(String)
+
+      expect(forecast.hourly_weather).to be_an(Array)
+      expect(forecast.hourly_weather.first).to be_a(Hash)
+      expect(forecast.hourly_weather.first.size).to eq(4)
+
+      expect(forecast.hourly_weather.first).to have_key(:time)
+      expect(forecast.hourly_weather.first[:time]).to be_a(String)
+
+      expect(forecast.hourly_weather.first).to have_key(:temperature)
+      expect(forecast.hourly_weather.first[:temperature]).to be_a(Float)
+
+      expect(forecast.hourly_weather.first).to have_key(:conditions)
+      expect(forecast.hourly_weather.first[:conditions]).to be_a(String)
+
+      expect(forecast.hourly_weather.first).to have_key(:icon)
+      expect(forecast.hourly_weather.first[:icon]).to be_a(String)
+    end
+  end
+end
