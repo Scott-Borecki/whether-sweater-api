13dd2293c6a086986bc95500e630f1f6b45621b6
Scott Borecki <scottborecki@gmail.com>
2021-09-26 13:34:56 -0600

    Add #json_response

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -1,4 +1,11 @@
 module Response
+  def json_response(object, status = :ok)
+    {
+      json: object,
+      status: status
+    }
+  end
+
   def json_error_response(object, status = :bad_request)
     {
       json: {
