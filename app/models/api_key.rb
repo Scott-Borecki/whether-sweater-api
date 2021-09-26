f284931ede74570d4048baa4aaeedab7c619032e
Scott Borecki <scottborecki@gmail.com>
2021-09-26 12:35:22 -0600

    Add .get_user_token

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -2,4 +2,8 @@ class ApiKey < ApplicationRecord
   belongs_to :user
 
   validates :token, presence: true, uniqueness: true, length: { is: 32 }
+
+  def self.get_user_token
+    first.token
+  end
 end
