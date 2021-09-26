611d2b8f844c75e246dd586fa7595bda0b665a68
Scott Borecki <scottborecki@gmail.com>
2021-09-25 18:29:13 -0600

    Create Image poro

---
+++ app/poros/image.rb
@@ -0,0 +1,18 @@
+class Image
+  attr_reader :image
+
+  def initialize(data)
+    @image = populate_image(data)
+  end
+
+  def populate_image(data)
+    {
+      location: data[:location][:name],
+      image_url: data[:urls][:full],
+      credit: {
+        source: data[:user][:links][:html],
+        author: data[:user][:username]
+      }
+    }
+  end
+end
---
+++ spec/support/image_response_body.rb
@@ -0,0 +1,83 @@
+# Endpoint response body recorded on September 25, 2021.
+def image_response_body
+  {:id=>"N0OOEpR1Lpo",
+   :created_at=>"2021-09-03T14:11:28-04:00",
+   :updated_at=>"2021-09-25T18:27:57-04:00",
+   :promoted_at=>"2021-09-04T16:24:01-04:00",
+   :width=>8512,
+   :height=>5696,
+   :color=>"#262626",
+   :blur_hash=>"L37A#q5QEd^8NbxaR+W-0x$+=|0x",
+   :description=>
+    "M8 & M20 respectivement la nébuleuse de la Lagune et la nébuleuse Trifide dans la constellation du Sagittaire.",
+   :alt_description=>"red and blue galaxy with stars",
+   :urls=>
+    {:raw=>
+      "https://images.unsplash.com/photo-1630692590101-c907a9efc124?ixid=MnwyNjMyMDd8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzI2MTQ3OTU&ixlib=rb-1.2.1",
+     :full=>
+      "https://images.unsplash.com/photo-1630692590101-c907a9efc124?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyNjMyMDd8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzI2MTQ3OTU&ixlib=rb-1.2.1&q=85",
+     :regular=>
+      "https://images.unsplash.com/photo-1630692590101-c907a9efc124?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjMyMDd8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzI2MTQ3OTU&ixlib=rb-1.2.1&q=80&w=1080",
+     :small=>
+      "https://images.unsplash.com/photo-1630692590101-c907a9efc124?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjMyMDd8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzI2MTQ3OTU&ixlib=rb-1.2.1&q=80&w=400",
+     :thumb=>
+      "https://images.unsplash.com/photo-1630692590101-c907a9efc124?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjMyMDd8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzI2MTQ3OTU&ixlib=rb-1.2.1&q=80&w=200"},
+   :links=>
+    {:self=>"https://api.unsplash.com/photos/N0OOEpR1Lpo",
+     :html=>"https://unsplash.com/photos/N0OOEpR1Lpo",
+     :download=>"https://unsplash.com/photos/N0OOEpR1Lpo/download",
+     :download_location=>
+      "https://api.unsplash.com/photos/N0OOEpR1Lpo/download?ixid=MnwyNjMyMDd8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzI2MTQ3OTU"},
+   :categories=>[],
+   :likes=>71,
+   :liked_by_user=>false,
+   :current_user_collections=>[],
+   :sponsorship=>nil,
+   :topic_submissions=>{},
+   :user=>
+    {:id=>"iLp3CiQFxrk",
+     :updated_at=>"2021-09-25T11:19:47-04:00",
+     :username=>"sam_astro",
+     :name=>"Samuel PASTEUR-FOSSE",
+     :first_name=>"Samuel",
+     :last_name=>"PASTEUR-FOSSE",
+     :twitter_username=>nil,
+     :portfolio_url=>nil,
+     :bio=>nil,
+     :location=>"Lailly-en-Val",
+     :links=>
+      {:self=>"https://api.unsplash.com/users/sam_astro",
+       :html=>"https://unsplash.com/@sam_astro",
+       :photos=>"https://api.unsplash.com/users/sam_astro/photos",
+       :likes=>"https://api.unsplash.com/users/sam_astro/likes",
+       :portfolio=>"https://api.unsplash.com/users/sam_astro/portfolio",
+       :following=>"https://api.unsplash.com/users/sam_astro/following",
+       :followers=>"https://api.unsplash.com/users/sam_astro/followers"},
+     :profile_image=>
+      {:small=>
+        "https://images.unsplash.com/profile-fb-1585063451-de5d389dc64b.jpg?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=32&w=32",
+       :medium=>
+        "https://images.unsplash.com/profile-fb-1585063451-de5d389dc64b.jpg?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=64&w=64",
+       :large=>
+        "https://images.unsplash.com/profile-fb-1585063451-de5d389dc64b.jpg?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=128&w=128"},
+     :instagram_username=>"sam_astrophoto",
+     :total_collections=>0,
+     :total_likes=>0,
+     :total_photos=>23,
+     :accepted_tos=>true,
+     :for_hire=>false,
+     :social=>
+      {:instagram_username=>"sam_astrophoto",
+       :portfolio_url=>nil,
+       :twitter_username=>nil,
+       :paypal_email=>nil}},
+   :exif=>{:make=>nil, :model=>nil, :exposure_time=>nil, :aperture=>nil, :focal_length=>nil, :iso=>nil},
+   :location=>
+    {:title=>"Lailly-en-Val, France",
+     :name=>"Lailly-en-Val, France",
+     :city=>"Lailly-en-Val",
+     :country=>"France",
+     :position=>{:latitude=>47.770374, :longitude=>1.68639}},
+   :views=>553921,
+   :downloads=>6351}
+end
