bf08ce28f15439127820e050dea98f4ca5b7a09c
Scott Borecki <scottborecki@gmail.com>
2021-09-26 11:49:08 -0600

    Update relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :api_keys
+
   validates :email, uniqueness: true, presence: true
 
   has_secure_password
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'relationships' do
+    it { should have_many(:api_keys) }
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
