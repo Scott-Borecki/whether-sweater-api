fd23f7b5b15a30abc11c09496a3d30ba52be0b0a
Scott Borecki <scottborecki@gmail.com>
2021-09-27 11:01:11 -0600

    Add sad path tests for Book Search endpoint

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::BookSearchController < ApplicationController
   before_action :validate_location, only: [:index]
+  before_action :validate_quantity, only: [:index]
 
   def index
     books = LibraryService.get_books_based_on_location(book_search_params)
@@ -16,6 +17,13 @@ class Api::V1::BookSearchController < ApplicationController
 
   def validate_location
     validator = LocationValidator.new(location: params[:location])
+    return if validator.valid?
+
+    render json_error_response(validator.errors, :bad_request)
+  end
+
+  def validate_quantity
+    validator = QuantityValidator.new(quantity: params[:quantity])
     return if validator.valid?
 
     render json_error_response(validator.errors, :bad_request)
--- spec/requests/api/v1/book_search_spec.rb
+++ spec/requests/api/v1/book_search_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
+# See spec/support/shared_examples/requests/ for shared examples
+# See spec/support/helpers/requests/request_spec_helper.rb for #json helper method
 RSpec.describe "Api::V1::BookSearches", type: :request do
   describe "GET /api/v1/book-search", :vcr do
     context 'when I provide valid parameters' do
@@ -22,18 +24,102 @@ RSpec.describe "Api::V1::BookSearches", type: :request do
 
     context 'when I do not provide valid parameters' do
       context 'when I do not provide a valid location' do
-        let(:quantity) { 5 }
-        let(:invalid_search_params) { { quantity: quantity } }
+        context 'when I do not provide a location' do
+          let(:quantity) { 5 }
+          let(:invalid_search_params) { { quantity: quantity } }
 
-        before { get '/api/v1/book-search', params: invalid_search_params }
+          before { get '/api/v1/book-search', params: invalid_search_params }
 
-        it 'returns a bad request error message' do
-          expect(json[:errors].size).to eq(1)
-          expect(json[:errors].first[:detail]).to eq("Location can't be blank")
+          it 'returns a bad request error message' do
+            expect(json[:errors].size).to eq(1)
+            expect(json[:errors].first[:detail]).to eq("Location can't be blank")
+          end
+
+          include_examples 'compliant json error format'
+          include_examples 'status code 400'
         end
 
-        include_examples 'compliant json error format'
-        include_examples 'status code 400'
+        context 'when I provide an empty location' do
+          let(:empty_location) { '' }
+          let(:quantity) { 5 }
+          let(:invalid_search_params) { { location: empty_location, quantity: quantity } }
+
+          before { get '/api/v1/book-search', params: invalid_search_params }
+          
+          it 'returns a bad request error message' do
+            expect(json[:errors].size).to eq(1)
+            expect(json[:errors].first[:detail]).to eq("Location can't be blank")
+          end
+
+          include_examples 'compliant json error format'
+          include_examples 'status code 400'
+        end
+      end
+
+      context 'when I do not provide a valid quantity' do
+        context 'when I do not provide a quantity' do
+          let(:location) { 'denver,co' }
+          let(:invalid_search_params) { { location: location } }
+
+          before { get '/api/v1/book-search', params: invalid_search_params }
+
+          it 'returns a bad request error message' do
+            expect(json[:errors].size).to eq(2)
+            expect(json[:errors].first[:detail]).to eq("Quantity can't be blank")
+            expect(json[:errors].second[:detail]).to eq('Quantity is not a number')
+          end
+
+          include_examples 'compliant json error format'
+          include_examples 'status code 400'
+        end
+
+        context 'when I provide an empty quantity' do
+          let(:location) { 'denver,co' }
+          let(:invalid_search_params) { { location: location, quantity: '' } }
+
+          before { get '/api/v1/book-search', params: invalid_search_params }
+
+          it 'returns a bad request error message' do
+            expect(json[:errors].size).to eq(2)
+            expect(json[:errors].first[:detail]).to eq("Quantity can't be blank")
+            expect(json[:errors].second[:detail]).to eq('Quantity is not a number')
+          end
+
+          include_examples 'compliant json error format'
+          include_examples 'status code 400'
+        end
+
+        context 'when I provide a negative quantity' do
+          let(:location) { 'denver,co' }
+          let(:negative_quantity) { -5 }
+          let(:invalid_search_params) { { location: location, quantity: negative_quantity } }
+
+          before { get '/api/v1/book-search', params: invalid_search_params }
+
+          it 'returns a bad request error message' do
+            expect(json[:errors].size).to eq(1)
+            expect(json[:errors].first[:detail]).to eq('Quantity must be greater than 0')
+          end
+
+          include_examples 'compliant json error format'
+          include_examples 'status code 400'
+        end
+
+        context 'when I provide a quantity of zero' do
+          let(:location) { 'denver,co' }
+          let(:zero_quantity) { 0 }
+          let(:invalid_search_params) { { location: location, quantity: zero_quantity } }
+
+          before { get '/api/v1/book-search', params: invalid_search_params }
+
+          it 'returns a bad request error message' do
+            expect(json[:errors].size).to eq(1)
+            expect(json[:errors].first[:detail]).to eq('Quantity must be greater than 0')
+          end
+
+          include_examples 'compliant json error format'
+          include_examples 'status code 400'
+        end
       end
     end
   end
