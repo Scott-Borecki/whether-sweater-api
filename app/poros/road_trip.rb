2d2c41f851ea6e1a8c49df136f1842872ae50337
Scott Borecki <scottborecki@gmail.com>
2021-09-27 14:08:28 -0600

    Create RoadTrip poro

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -1,22 +1,24 @@
 class RoadTrip
-  attr_reader :start_city, :end_city, :travel_time, :weather_at_eta
+  attr_reader :id, :start_city, :end_city, :travel_time, :weather_at_eta
 
   def initialize(directions, forecast)
-    @start_city     = origin
-    @end_city       = destination
-    @travel_time    = travel_time_formatted
-    @weather_at_eta = populate_weather(data[:xxx])
+    @id             = nil
+    @start_city     = origin(directions[:route][:locations].first[:adminArea5],
+                             directions[:route][:locations].first[:adminArea3])
+    @end_city       = destination(
+                        directions[:route][:locations].last[:adminArea5],
+                        directions[:route][:locations].last[:adminArea3]
+                      )
+    @travel_time    = estimated_travel_time(directions[:route][:realTime])
+    @weather_at_eta = populate_weather_forecast(directions[:route][:realTime],
+                                                forecast)
   end
 
-  def origin
-    city = directions[:route][:locations].first[:adminArea5]
-    state = directions[:route][:locations].first[:adminArea3]
+  def origin(city, state)
     "#{city}, #{state}"
   end
 
-  def destination
-    city = directions[:route][:locations].last[:adminArea5]
-    state = directions[:route][:locations].last[:adminArea3]
+  def destination(city, state)
     "#{city}, #{state}"
   end
 
@@ -24,10 +26,9 @@ class RoadTrip
   # conditions and speeds (where available) along with historical traffic
   # conditions.  This estimate is most useful on short routes where current
   # conditions produce a higher percentage impact to the total route time.
-  def estimated_travel_time
-    total_seconds = directions[:route][:realTime]
-    hours = total_seconds / 3600
-    minutes = total_seconds % 3600
+  def estimated_travel_time(travel_time_seconds)
+    hours = travel_time_seconds / 3600
+    minutes = travel_time_seconds / 60 % 60
 
     if hours.zero?
       "#{minutes} minutes"
@@ -39,15 +40,23 @@ class RoadTrip
   # Returns the calculated elapsed time for the route, without consideration
   # for current traffic conditions, current traffic speeds, or historical
   # traffic conditions.
-  def calculated_travel_time
-    total_seconds = directions[:route][:time]
-    hours = total_seconds / 3600
-    minutes = total_seconds % 3600
+  def calculated_travel_time(travel_time_seconds)
+    hours = travel_time_seconds / 3600
+    minutes = travel_time_seconds / 60 % 60
 
     if hours.zero?
       "#{minutes} minutes"
     else
       "#{hours} hours, #{minutes} minutes"
     end
+  end
+
+  def populate_weather_forecast(travel_time_seconds, forecast)
+    hour_index = travel_time_seconds / 3600
+
+    {
+      conditions: forecast[:hourly][hour_index][:weather].first[:description],
+      temperature: forecast[:hourly][hour_index][:temp]
+    }
   end
 end
--- spec/poros/road_trip_spec.rb
+++ spec/poros/road_trip_spec.rb
@@ -3,40 +3,46 @@ require 'rails_helper'
 describe RoadTrip, type: :poro do
   describe 'object creation' do
     context 'when the route is possible' do
-      xit 'is valid and has readable attributes' do
-        # See spec/support/road_trip_response_body.rb for #road_trip_response_body
-        data = road_trip_response_body
-        road_trip = RoadTrip.new(data)
+      it 'is valid and has readable attributes' do
+        # See spec/support/directions_response_body.rb for #directions_response_body
+        directions = directions_response_body
+        # See spec/support/forecast_imperial_response_body.rb for #forecast_imperial_response_body
+        forecast = forecast_imperial_response_body
+        road_trip = RoadTrip.new(directions, forecast)
 
         expect(road_trip).to be_a(RoadTrip)
+        
+        expect(road_trip.id).to be_nil
 
         expect(road_trip.start_city).to be_a(String)
         expect(road_trip.start_city).to eq('Denver, CO')
 
         expect(road_trip.end_city).to be_a(String)
-        expect(road_trip.end_city).to eq('Estes Park, CO')
+        expect(road_trip.end_city).to eq('Pueblo, CO')
 
         expect(road_trip.travel_time).to be_a(String)
-        expect(road_trip.travel_time).to eq('2 hours, 13 minutes')
+        expect(road_trip.travel_time).to eq('1 hours, 51 minutes')
 
         expect(road_trip.weather_at_eta).to be_a(Hash)
         expect(road_trip.weather_at_eta.size).to eq(2)
 
         expect(road_trip.weather_at_eta).to have_key(:temperature)
         expect(road_trip.weather_at_eta[:temperature]).to be_a(Numeric)
-        expect(road_trip.weather_at_eta[:temperature]).to eq(59.4)
+        expect(road_trip.weather_at_eta[:temperature]).to eq(56.52)
 
         expect(road_trip.weather_at_eta).to have_key(:conditions)
         expect(road_trip.weather_at_eta[:conditions]).to be_a(String)
-        expect(road_trip.weather_at_eta[:conditions]).to eq('partly cloudy with a chance of meatballs')
+        expect(road_trip.weather_at_eta[:conditions]).to eq('clear sky')
       end
     end
 
     context 'when the route is impossible' do
       xit 'is valid and has readable attributes' do
-        # See spec/support/road_trip_response_body.rb for #road_trip_response_body
-        data = road_trip_response_body
-        road_trip = RoadTrip.new(data)
+        # See spec/support/directions_response_body.rb for #directions_response_body
+        directions = directions_response_body
+        # See spec/support/forecast_imperial_response_body.rb for #forecast_imperial_response_body
+        forecast = forecast_imperial_response_body
+        road_trip = RoadTrip.new(directions, forecast)
 
         expect(road_trip).to be_a(RoadTrip)
 
